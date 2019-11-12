50d422b5d8e2a615e3b01bea84093877853c1bc1
scepbjoern <scep@zhaw.ch>
2019-11-12 07:42:48 +0100

    Initial commit

---
+++ src/main/java/ch/zhaw/iwi/sd/rest/RestServiceApplication.java
@@ -0,0 +1,13 @@
+package ch.zhaw.iwi.sd.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestServiceApplication.class, args);
+	}
+
+}
